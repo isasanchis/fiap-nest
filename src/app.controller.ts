ecb6b3d69ed6ec6b4ae16ecbbd1d62752f0e3c75
Isabella Sanchis <isasanchis3@gmail.com>
2022-10-24 11:45:00 -0300

    Controller e service configurados

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,19 @@ import { AppService } from './app.service'; // injeção de dependência
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/users')
   getHello(): string {
+    // está apenas redirecionando o usuário para o serviço correto
     return this.appService.getHello();
+  }
+
+  @Get('/login')
+  getLogin(): string {
+    return this.appService.getLogin();
+  }
+
+  @Get('/register')
+  getRegister(): string {
+    return this.appService.getRegister();
   }
 }
