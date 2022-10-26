8bdeeb657e7f40900260d8357ef7f05124883b17
Isabella Sanchis <isasanchis3@gmail.com>
2022-10-26 10:11:09 -0300

    Controller e service do Users criados

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Delete, Patch } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDTO } from './dto/createUser.dto';
+import { ParseUUIDPipe } from '@nestjs/common/pipes';
+import { UpdateUserDTO } from './dto/updateUser.dto';
 
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
+
+    @Post()
+    create(@Body() req: CreateUserDTO) {
+        return this.usersService.create();
+    }
+
+    @Get()
+    findAll() {
+        return this.usersService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id', ParseUUIDPipe) id: number) {
+        return this.usersService.findOne(id);
+    }
+
+    @Patch(':id')
+    update(@Param('id', ParseUUIDPipe) id: number,
+        @Body() req: UpdateUserDTO) {
+            return this.usersService.update(id, req);
+    }
+
+    @Delete(':id')
+    deleteOne(@Param('id') id: number) {
+        return this.usersService.delete(id);
+    }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { UpdateUserDTO } from './dto/updateUser.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    async create(): Promise<string> { // retorna promessa pois pode não dar certo
+        return 'Usuário criado com sucesso!';
+    }
+
+    async findAll(): Promise<string> {
+        return 'Lista de usuários!';
+    }
+
+    async findOne(id: number): Promise<string> {
+        return `Usuário ${id}!`;
+    }
+
+    async update(id: number, req: UpdateUserDTO): Promise<string> {
+        return `Usuário ${id} atualizado com sucesso!`;
+    }
+
+    async delete(id: number): Promise<string> {
+        return `Usuário ${id} deletado com sucesso!`;
+    }
+}
