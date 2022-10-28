57b0101e76a4d549aa7b028d075e0cca0d99eca3
Isabella Sanchis <isasanchis3@gmail.com>
2022-10-28 10:40:05 -0300

    Prisma configs 2

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
 import { UpdateUserDTO } from './dto/updateUser.dto';
 
 @Injectable()
 export class UsersService {
+    constructor(private prisma:PrismaService) {}
+    
     async create(): Promise<string> { // retorna promessa pois pode não dar certo
         return 'Usuário criado com sucesso!';
     }
