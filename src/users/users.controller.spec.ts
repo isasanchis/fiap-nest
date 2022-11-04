b654116fa2b0d7c45599145421287a24d71043c5
Isabella Sanchis <isasanchis3@gmail.com>
2022-11-04 09:50:22 -0300

    Testes

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,12 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { EmailModule } from 'src/email/email.module';
+import { EmailService } from 'src/email/email.service';
+import { PrismaService } from 'src/prisma.service';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [EmailModule],
       controllers: [UsersController],
+      providers: [UsersService, EmailService, PrismaService]
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
